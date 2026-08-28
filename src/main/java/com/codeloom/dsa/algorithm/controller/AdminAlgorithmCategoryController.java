4c0b97a7e77978b1f21c542c28f59854fc846244
Sukhankar <sukhankarsh@gmail.com>
2026-08-28 21:44:21 +0530

    fix(backend): re-add io.swagger.v3.oas.annotations.tags.Tag import to AdminAlgorithmCategoryController

--- src/main/java/com/codeloom/dsa/algorithm/controller/AdminAlgorithmCategoryController.java
+++ src/main/java/com/codeloom/dsa/algorithm/controller/AdminAlgorithmCategoryController.java
@@ -9,7 +9,9 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 
 
