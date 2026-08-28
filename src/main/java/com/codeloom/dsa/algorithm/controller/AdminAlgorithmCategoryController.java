77a0bde174a644a35a396e77b3363d69a5147c13
Sukhankar <sukhankarsh@gmail.com>
2026-08-28 21:43:03 +0530

    fix(backend): re-add jakarta.validation.Valid import to AdminAlgorithmCategoryController

--- src/main/java/com/codeloom/dsa/algorithm/controller/AdminAlgorithmCategoryController.java
+++ src/main/java/com/codeloom/dsa/algorithm/controller/AdminAlgorithmCategoryController.java
@@ -9,8 +9,9 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
